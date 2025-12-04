b0fb1b8a3813a236df6688387a6c277651aa3d17
Joshua <jaraherrerajosh@gmail.com>
2025-12-03 23:28:22 -0300

    feat(front): agregar componente taller

---
+++ Front-End/src/app/taller/taller.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taller',
+  templateUrl: './taller.component.html',
+  styleUrls: ['./taller.component.css']
+})
+export class TallerComponent {
+
+}
